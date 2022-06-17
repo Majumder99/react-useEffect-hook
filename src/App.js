d628ec19ad59b5015f11f5c4c77fee71604a9171
Sourav Majumder <96306119+Majumder99@users.noreply.github.com>
2022-06-17 12:26:54 +0600

    new

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect } from "react";
+import { useState } from "react";
 
 function App() {
+  const [width, setWidth] = useState(window.innerWidth);
+  const handleResize = () => {
+    console.log("Setting the width");
+    setWidth(window.innerWidth);
+  };
+  useEffect(() => {
+    window.addEventListener("resize", handleResize);
+  }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div>{width}</div>
+    </>
   );
+
+  // const [resourceType, setResourceType] = useState("post");
+  // const [items, setItems] = useState([]);
+  // useEffect(() => {
+  //   // console.log("Render");
+  //   fetch(`https://jsonplaceholder.typicode.com/${resourceType}`)
+  //     .then((response) => response.json())
+  //     .then((json) => setItems(json));
+  //   // .then((data) => setItems(data));
+  // }, [resourceType]);
+  // return (
+  //   <>
+  //     <div>
+  //       <button onClick={() => setResourceType("posts")}>Post</button>
+  //       <button onClick={() => setResourceType("comments")}>Comments</button>
+  //       <button onClick={() => setResourceType("users")}>Users</button>
+  //       <h1>{resourceType}</h1>
+  //       {items.map((item) => {
+  //         return <pre>{JSON.stringify(item)}</pre>;
+  //       })}
+  //     </div>
+  //   </>
+  // );
 }
 
 export default App;
